0cd643d75940784128741519a5b0ad8e3bb0ffa3
HaixiongLi <34835300+HaixiongLi@users.noreply.github.com>
2020-01-14 13:48:22 +0800

    简单读取json的代码

---
+++ readjson.cpp
@@ -0,0 +1,21 @@
+#include "json.hpp"    // https://github.com/nlohmann/json/tree/develop/single_include/nlohmann/json.hpp
+#include <fstream>              // ifstream, ofstream
+#include <iomanip>              // std::setw()
+#include <iostream>
+using json = nlohmann::json;
+void main() {
+
+	std::ifstream fin("config.json");   // 注意此处是相对路径
+	json j;
+	fin >> j;
+	fin.close();
+
+	// 检查读取的和预期的是否一致
+	std::cout << j["name"] << std::endl;	// 
+	std::cout << j["Camera_size"] << std::endl;	// 
+	std::cout << j["M1"] << std::endl;	// 
+	std::cout << j["D1"] << std::endl;	//
+	std::cout << j["M2"] << std::endl;	// 
+	std::cout << j["D2"] << std::endl;	// 
+
+}
